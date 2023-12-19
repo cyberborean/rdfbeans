bf6eb5aa05499aed594812720a31f921cd811296
chicoreus <mole@morris.net>
2023-12-19 15:14:40 -0500

    Moving to current RDF4J version.  Updating pom to rdf4j 4.3.8 (and current slf4j 2.0.9).  With RDF4j 4.0, RDF4JException moved, updating references to new path: org.eclipse.rdf4j.common.exception.RDF4JException.  Also commenting out invocations of initialize() on SailRepository, Repository no longer has an exposed initialized method.

--- src/main/java/org/cyberborean/rdfbeans/proxy/RDFBeanDelegator.java
+++ src/main/java/org/cyberborean/rdfbeans/proxy/RDFBeanDelegator.java
@@ -22,7 +22,7 @@ import org.cyberborean.rdfbeans.exceptions.RDFBeanException;
 import org.cyberborean.rdfbeans.exceptions.RDFBeanValidationException;
 import org.cyberborean.rdfbeans.reflect.RDFBeanInfo;
 import org.cyberborean.rdfbeans.reflect.RDFProperty;
-import org.eclipse.rdf4j.RDF4JException;
+import org.eclipse.rdf4j.common.exception.RDF4JException;
 import org.eclipse.rdf4j.common.iteration.CloseableIteration;
 import org.eclipse.rdf4j.model.BNode;
 import org.eclipse.rdf4j.model.IRI;
